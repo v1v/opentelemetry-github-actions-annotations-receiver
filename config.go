317901231e368e6ff01a53860ebb5e28b3874e3e
Victor Martinez <victormartinezrubio@gmail.com>
2024-10-15 15:58:44 +0200

    feat: send annotations (#1)

--- config.go
+++ config.go
@@ -1,6 +1,8 @@
 package opentelemetrygithubactionsannotationsreceiver
 
 import (
+	"time"
+
 	"go.opentelemetry.io/collector/config/confighttp"
 	"go.opentelemetry.io/collector/config/configopaque"
 )
@@ -10,4 +12,15 @@ type Config struct {
 	Path                    string
 	WebhookSecret           configopaque.String `mapstructure:"webhook_secret"`
 	Token                   configopaque.String `mapstructure:"token"`
+	Retry                   RetryConfig         `mapstructure:"retry"`
+	BatchSize               int                 `mapstructure:"batch_size"`
+	CustomServiceName       string              `mapstructure:"custom_service_name"`
+	ServiceNamePrefix       string              `mapstructure:"service_name_prefix"`
+	ServiceNameSuffix       string              `mapstructure:"service_name_suffix"`
+}
+
+type RetryConfig struct {
+	InitialInterval time.Duration `mapstructure:"initial_interval"`
+	MaxInterval     time.Duration `mapstructure:"max_interval"`
+	MaxElapsedTime  time.Duration `mapstructure:"max_elapsed_time"`
 }
